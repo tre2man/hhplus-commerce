dfb4db201b1da7c530814164bc2eb454993ea4db
tre2man <kimtree3940@gmail.com>
2025-08-05 18:32:30 +0900

    feature: transaction 처

--- src/main/java/kr/hhplus/be/server/domain/coupon/facade/CouponFacade.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/facade/CouponFacade.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.domain.coupon.facade;
 
+import jakarta.transaction.Transactional;
 import kr.hhplus.be.server.domain.coupon.command.CreateIssuedCouponCommand;
 import kr.hhplus.be.server.domain.coupon.service.CouponService;
 import kr.hhplus.be.server.domain.coupon.service.IssuedCouponService;
@@ -15,6 +16,7 @@ public class CouponFacade {
     private final CouponService couponService;
     private final IssuedCouponService issuedCouponService;
 
+    @Transactional
     public void issueCoupon(Long userId, Long couponId) {
         Integer expireDay = couponService.getCouponExpireDay(couponId);
         issuedCouponService.createIssuedCoupon(
--- src/main/java/kr/hhplus/be/server/domain/product/facade/ProductFacade.java
+++ src/main/java/kr/hhplus/be/server/domain/product/facade/ProductFacade.java
@@ -4,11 +4,11 @@ import kr.hhplus.be.server.domain.order.service.OrderProductService;
 import kr.hhplus.be.server.domain.product.entity.Product;
 import kr.hhplus.be.server.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class ProductFacade {
     private final ProductService productService;
