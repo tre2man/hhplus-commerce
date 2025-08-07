4d8331a17fe93c210b2ba05f248327c0f190c2a0
tre2man <kimtree3940@gmail.com>
2025-08-07 23:36:02 +0900

    feature: 상품 재고 감소 동시성

--- src/main/java/kr/hhplus/be/server/domain/product/service/ProductService.java
+++ src/main/java/kr/hhplus/be/server/domain/product/service/ProductService.java
@@ -5,6 +5,7 @@ import kr.hhplus.be.server.domain.product.entity.Product;
 import kr.hhplus.be.server.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,14 +19,20 @@ public class ProductService {
                 .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다. productId: " + productId));
     }
 
+    private Product getProductByIdForUpdate(Long productId) {
+        return this.productRepository.findByIdForUpdate(productId)
+                .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다. productId: " + productId));
+    }
+
     public List<Product> getProductsByIds(List<Long> productIds) {
         return this.productRepository.findAllById(productIds);
     }
 
+    @Transactional
     public void decreaseStock(List<OrderProductCommand> command) {
         for (OrderProductCommand orderProductCommand : command) {
             Long productId = orderProductCommand.productId();
-            Product product = this.getProductById(productId);
+            Product product = this.getProductByIdForUpdate(productId);
 
             Integer quantity = orderProductCommand.quantity();
             product.decreaseStock(quantity);
