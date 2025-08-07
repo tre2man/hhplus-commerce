6bea07fb222a0b0cedb85445d24619f73dcbc5f6
tre2man <kimtree3940@gmail.com>
2025-08-07 23:36:02 +0900

    feature: 상품 재고 감소 동시성

--- src/main/java/kr/hhplus/be/server/domain/product/repository/ProductRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/product/repository/ProductRepository.java
@@ -1,8 +1,17 @@
 package kr.hhplus.be.server.domain.product.repository;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> { }
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM product p WHERE p.id = :id")
+    Optional<Product> findByIdForUpdate(Long id);
+}
