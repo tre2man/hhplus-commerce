cff227bbfe84c1def14f012194f9f5853eead020
tre2man <kimtree3940@gmail.com>
2025-08-07 23:28:17 +0900

    feature: 쿠폰 비관적 락 구현

--- src/main/java/kr/hhplus/be/server/domain/coupon/repository/CouponRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/repository/CouponRepository.java
@@ -1,8 +1,17 @@
 package kr.hhplus.be.server.domain.coupon.repository;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.coupon.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CouponRepository extends JpaRepository<Coupon, Long> { }
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM coupon c WHERE c.id = :id")
+    Optional<Coupon> findByIdForUpdate(Long id);
+}
