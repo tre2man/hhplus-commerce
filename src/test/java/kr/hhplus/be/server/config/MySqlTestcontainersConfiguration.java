facd1a6d48bdee76a57e46f64db50c0d149f0a23
tre2man <kimtree3940@gmail.com>
2025-08-14 17:32:58 +0900

    feature: 분산 락 구현

--- src/test/java/kr/hhplus/be/server/config/MySqlTestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/config/MySqlTestcontainersConfiguration.java
@@ -1,15 +1,12 @@
-package kr.hhplus.be.server;
+package kr.hhplus.be.server.config;
 
 import jakarta.annotation.PreDestroy;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @Configuration
-class TestcontainersConfiguration {
+class MySqlTestcontainersConfiguration {
 
 	public static final MySQLContainer<?> MYSQL_CONTAINER;
 
