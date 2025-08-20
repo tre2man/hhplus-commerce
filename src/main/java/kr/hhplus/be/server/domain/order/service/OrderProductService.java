bb7238e66f9de9df3c79ace572b449be1cc5b6e2
tre2man <kimtree3940@gmail.com>
2025-08-20 22:37:47 +0900

    remove: 사용하지 않는 로

--- src/main/java/kr/hhplus/be/server/domain/order/service/OrderProductService.java
+++ src/main/java/kr/hhplus/be/server/domain/order/service/OrderProductService.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,20 +20,5 @@ public class OrderProductService {
                 .map(cmd -> OrderProduct.create(orderId, cmd.productId(), cmd.quantity()))
                 .toList();
         this.orderProductRepository.saveAll(orderProductList);
-    }
-
-    // 3일 내 주문량 상위 5개 상품의 아이디를 반환합니다.
-    public List<Long> getPopular5ProductIds() {
-        List<OrderProduct> orderProductList = this.orderProductRepository.findAllByCreatedAtAfter(LocalDateTime.now().minusDays(3));
-        return orderProductList.stream()
-                .collect(Collectors.groupingBy(
-                        OrderProduct::getProductId,
-                        Collectors.summingInt(OrderProduct::getQuantity)
-                ))
-                .entrySet().stream()
-                .sorted(Map.Entry.<Long, Integer>comparingByValue().reversed())
-                .limit(5)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
     }
 }
