053301f2c3229230d04aca0c930a877bd3a282be
tre2man <kimtree3940@gmail.com>
2025-08-20 22:37:33 +0900

    feature: 인기 상품 로직 스케줄러

--- src/main/java/kr/hhplus/be/server/scheduler/PopularProductCacheScheduler.java
+++ src/main/java/kr/hhplus/be/server/scheduler/PopularProductCacheScheduler.java
@@ -1,27 +1,18 @@
 package kr.hhplus.be.server.scheduler;
 
-import kr.hhplus.be.server.domain.product.facade.ProductFacade;
+import kr.hhplus.be.server.domain.dataplatform.service.DataPlatformService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class PopularProductCacheScheduler {
-    private final ProductFacade productFacade;
-    private final CacheManager cacheManager;
+    private final DataPlatformService dataPlatformService;
 
-    // 매일 1시간마다 인기 상품 캐시를 갱신합니다.
-    @Scheduled(cron = "0 0 * * * ?")
+    // 매일 10분마다 인기 상품 캐시를 업데이트합니다.
+    @Scheduled(cron = "0 0/10 * * * ?")
     public void updatePopularProductCache() {
-            // 캐시를 무효화합니다.
-            Cache existCache = cacheManager.getCache("PRODUCT::POPULAR");
-            if (existCache != null) {
-                existCache.clear();
-            }
-            // 인기 상품 캐시를 갱신합니다.
-            productFacade.getPopularProducts();
+        dataPlatformService.updateTopNOrderProducts(5);
     }
 }
