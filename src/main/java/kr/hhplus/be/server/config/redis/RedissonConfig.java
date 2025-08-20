3124c2f345c2b76d3bf73536ff5208f1d5b26cfb
tre2man <kimtree3940@gmail.com>
2025-08-20 22:34:58 +0900

    feature: redis config 통합

--- src/main/java/kr/hhplus/be/server/config/redis/RedissonConfig.java
+++ src/main/java/kr/hhplus/be/server/config/redis/RedissonConfig.java
@@ -1,11 +1,23 @@
 package kr.hhplus.be.server.config.redis;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedissonConfig {
@@ -17,12 +29,57 @@ public class RedissonConfig {
 
     private static final String REDISSON_HOST_PREFIX = "redis://";
 
+    private static final Long DEFAULT_EXPIRATION_MIN = 60L;
+
     @Bean
     public RedissonClient redissonClient() {
-        RedissonClient redisson = null;
         Config config = new Config();
         config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
-        redisson = Redisson.create(config);
-        return redisson;
+        return Redisson.create(config);
     }
+
+    @Bean
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper om = new ObjectMapper();
+        om.findAndRegisterModules();
+        om.configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true);
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return om;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf, ObjectMapper redisObjectMapper) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(cf);
+
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+
+        template.setKeySerializer(keySerializer);
+        template.setHashKeySerializer(keySerializer);
+        template.setValueSerializer(valueSerializer);
+        template.setHashValueSerializer(valueSerializer);
+        template.setDefaultSerializer(valueSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory cf, ObjectMapper redisObjectMapper) {
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer))
+                .computePrefixWith(cacheName -> "CACHE::" + cacheName + "::")
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofMinutes(DEFAULT_EXPIRATION_MIN));
+
+        return RedisCacheManager.builder(cf)
+                .cacheDefaults(cacheConfig)
+                .transactionAware()
+                .build();
+    }
+
 }
